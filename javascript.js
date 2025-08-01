860e83710b47c48d3ed37d2318175df295e1d400
Student name <roudosisevang@gmail.com>
2025-08-01 20:47:08 +0300

    New JAvaScript content update

--- javascript.js
+++
@@ -1,3 +0,0 @@
-let variable = "Hello World !";
-let toUpperCase = variable.toUpperCase();
-console.log(toUpperCase);
---
+++ main.js
@@ -0,0 +1,41 @@
+/* let variable = "Hello World !";
+let toUpperCase = variable.toUpperCase();
+console.log(toUpperCase);
+ */
+/* ---------------------------------------- */
+
+
+/* function greeting(name) {
+  console.log("Hello, " + name + " Web Developer!");
+}
+
+greeting("Evangelos");
+greeting("Roudosis");
+ */
+
+/* const greetings = (name) => 
+  { console.log("Hello, " + name + " !")
+}
+
+greetings("Evangelos"); */
+/* ----------------------------------------------- */
+
+/* 
+
+const calculateArea = (width, height) => {
+  const area = width * height;
+  return area;
+}
+console.log(calculateArea(5, 3)); */
+
+let age =  16;
+
+  if (age >= 18) {
+    console.log("You are an adult.");
+  } else if (age >= 15 && age < 65) {
+    console.log("You are a yound adult.");
+  } else if (age < 15 && age >= 0) {
+    console.log("You must be above 18 years old.");
+  } else {
+    console.log("Invalid.");
+  }
